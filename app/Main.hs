c1d02cc4de885c52e4c6e9c2f1ab7f767706a8c6
aemaeth <lingoerer@gmail.com>
2026-04-25 21:27:01 +0800

    term

--- app/Main.hs
+++ app/Main.hs
@@ -19,7 +19,7 @@ main = do
         focusedBorderColor = T.primaryColor,
         normalBorderColor = T.backgroundColor,
         borderWidth = 2,
-        terminal = "kitty -1",
+        terminal = "alacritty",
         layoutHook = avoidStruts (smartBorders $ Tall 1 0.03 0.62) ||| noBorders Full,
         focusFollowsMouse = False
       }
